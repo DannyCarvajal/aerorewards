d30e81c1423cba7213559eccf4505d78b6fc38fa
Oliver Daniel <juan-daniel1914@hotmail.com>
2021-12-14 22:28:36 -0500

    Adding globalState with the context API for points

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
 // Styles
 import { ThemeProvider } from "styled-components";
-import { COLORS } from "constants/globalStyles";
+import { GlobalStyles, COLORS } from "constants/globalStyles";
 // Pages
-import Home from "pages/home/Home";
-import Products from "pages/redeemProducts/Products";
-import BetMachine from "pages/slotMachine/BetMachine";
+import Home from "pages/Home";
+import Products from "pages/Products";
+import BetMachine from "pages/BetMachine";
 // Router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// Components
+import Navbar from "components/Navbar/index";
+// Context
+import PointsProvider from "context/pointsContext";
 
 function App() {
 	return (
-		<ThemeProvider theme={COLORS}>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/products" element={<Products />} />
-					<Route path="/game" element={<BetMachine />} />
-				</Routes>
-			</BrowserRouter>
-		</ThemeProvider>
+		<PointsProvider>
+			<ThemeProvider theme={COLORS}>
+				<GlobalStyles />
+				<Navbar />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/products" element={<Products />} />
+						<Route path="/game" element={<BetMachine />} />
+						<Route path="*" element={<Navigate to="/" />} />
+					</Routes>
+				</BrowserRouter>
+			</ThemeProvider>
+		</PointsProvider>
 	);
 }
 
